ad0fc5d84290908e9f9891dbfc78ef978808dd4e
Gabriel Terrazas <95561135+Kombini21@users.noreply.github.com>
2022-06-26 16:34:48 -0400

    Create app logic and format the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
+      return const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: MyHomePage(),
       );
@@ -24,11 +25,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  int _seconds = 0;
+  int _minutes = 25;
+  Timer _timer = Timer(const Duration(seconds: 1), () {});
+
+  void _stopTimer(){
+
+    _timer.cancel();
+    _seconds = 0;
+    _minutes = 25;
+
+  }
+
+  void _startTimer(){
+
+    // ignore: unnecessary_null_comparison
+    if (_timer != null){
+      _timer.cancel();
+    }
+    if (_minutes > 0 ){
+      _seconds = _minutes * 60;
+    }
+    if (_seconds > 60) {
+
+      _minutes = (_seconds/60).floor();
+      _seconds -= (_minutes * 60);
+    }
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_seconds > 0){
+          _seconds--;
+        }
+        else {
+          if (_minutes > 0){
+            _seconds=59;
+            _minutes--;
+          }
+          else {
+            _timer.cancel();
+            // ignore: avoid_print
+            print("Timer completo.");
+          }
+        }
+      });
+     });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
-      backgroundColor: Color.fromRGBO(27, 52, 75, 1),
+      backgroundColor: const Color.fromRGBO(27, 52, 75, 1),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -39,20 +89,31 @@ class _MyHomePageState extends State<MyHomePage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "25:00",
-                style: TextStyle(
+                "$_minutes : $_seconds",
+                style: const TextStyle(
                   color: Colors.redAccent,
                   fontSize: 48,
                 ),
               ),
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 300,
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
+                onPressed: (){
+                  _stopTimer();
+                },
+                style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                padding: const EdgeInsets.all(40.0),
+                shape: const CircleBorder(
+                  side: BorderSide(color: Colors.orange),
+                ),
+                ),
                 child: const Text(
                   "Stop",
                   style: TextStyle(
@@ -60,18 +121,18 @@ class _MyHomePageState extends State<MyHomePage> {
                     fontSize: 24,
                   ),
                   ),
+              ),
+              ElevatedButton(
                 onPressed: (){
-                  //_StopTimer():
+                  _startTimer();
                 },
                 style: ElevatedButton.styleFrom(
                 primary: Colors.black,
                 padding: const EdgeInsets.all(40.0),
-                shape: CircleBorder(
+                shape: const CircleBorder(
                   side: BorderSide(color: Colors.orange),
                 ),
                 ),
-              ),
-              ElevatedButton(
                 child: const Text(
                   "Start",
                   style: TextStyle(
@@ -79,16 +140,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     fontSize: 24,
                   ),
                   ),
-                onPressed: (){
-                  //_StopTimer():
-                },
-                style: ElevatedButton.styleFrom(
-                primary: Colors.black,
-                padding: const EdgeInsets.all(40.0),
-                shape: CircleBorder(
-                  side: BorderSide(color: Colors.orange),
-                ),
-                ),
               ),
             ],
           )
